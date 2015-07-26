1838b762bdd12deaa2923203b755824af51a1766
t_elle_jay <theresaj@gmail.com>
2015-07-26 11:13:49 -0700

    Step 5 easier

--- getting-and-cleaning-data/run_analysis.R
+++ getting-and-cleaning-data/run_analysis.R
@@ -2,56 +2,61 @@
 
 # 1. Merges the training and the test sets to create one data set.
 
-temp1 <- read.table("train/X_train.txt")
-temp2 <- read.table("test/X_test.txt")
-X <- rbind(temp1, temp2)
 
-temp1 <- read.table("train/subject_train.txt")
-temp2 <- read.table("test/subject_test.txt")
-S <- rbind(temp1, temp2)
+x_train <- read.table("train/X_train.txt")
+y_train <- read.table("train/y_train.txt")
+subject_train <- read.table("train/subject_train.txt")
 
-temp1 <- read.table("train/y_train.txt")
-temp2 <- read.table("test/y_test.txt")
-Y <- rbind(temp1, temp2)
+x_test <- read.table("test/X_test.txt")
+y_test <- read.table("test/y_test.txt")
+subject_test <- read.table("test/subject_test.txt")
 
-# 2. Extracts the mean and standard deviation for each measurement.
+# create 'x' data set
+X <- rbind(x_train, x_test)
+
+# create 'y' data set
+Y <- rbind(y_train, y_test)
+
+# create 'subject' data set
+S <- rbind(subject_train, subject_test)
+
+# 2. Extract only the measurements on the mean and standard deviation for each measurement.
 
 features <- read.table("features.txt")
-indices_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
-X <- X[, indices_of_good_features]
-names(X) <- features[indices_of_good_features, 2]
+
+# get only columns with mean() or std() in their names
+indices_of_mean_std_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+
+# subset the desired columns
+X <- X[, indices_of_mean_std_features]
+
+# correct the column names
+names(X) <- features[indices_of_mean_std_features, 2]
 names(X) <- gsub("\\(|\\)", "", names(X))
 names(X) <- tolower(names(X))
 
-# 3. Name the activities in the data set.
+# 3. Use descriptive activity names to name the activities in the data set.
 
 activities <- read.table("activity_labels.txt")
 activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
+
+# update values with correct activity names
 Y[,1] = activities[Y[,1], 2]
+
+# correct column name
 names(Y) <- "activity"
 
-# 4. Label the data set.
+# 4. Appropriately label the data set with descriptive variable names.
 
+# correct column name
 names(S) <- "subject"
+
+# bind all the data in a single data set
 cleaned <- cbind(S, Y, X)
 write.table(cleaned, "merged_clean_data.txt")
 
-# 5. Create independent tidy data set with the average of each variable for each activity and each subject.
+# 5. Create a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-uniqueSubjects = unique(S)[,1]
-numSubjects = length(unique(S)[,1])
-numActivities = length(activities[,1])
-numCols = dim(cleaned)[2]
-result = cleaned[1:(numSubjects*numActivities), ]
+result <- ddply(cleaned, .(subject, activity), function(x) colMeans(x[, 1:66]))
 
-row = 1
-for (s in 1:numSubjects) {
-    for (a in 1:numActivities) {
-        result[row, 1] = uniqueSubjects[s]
-        result[row, 2] = activities[a, 2]
-        tmp <- cleaned[cleaned$subject==s & cleaned$activity==activities[a, 2], ]
-        result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
-        row = row+1
-    }
-}
 write.table(result, "data_set_with_the_averages.txt")
